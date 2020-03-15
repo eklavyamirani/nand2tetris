28734da7f399350e3b4bc05bd28f3ed231690ce2
Eklavya Mirani <eklavyamirani@hotmail.com>
2020-03-14 18:10:34 -0700

    complete multiplication

--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,53 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+
+    // var multiplicand = R0;
+    // var multiplier = R1;
+    // var result = 0;
+
+    // for (int i=1; i <= multiplier; ++i)
+    // {
+    //     result = result + multiplicand;
+    // }
+
+    // R2 = result;
+
+// result = 0
+@R2
+M = 0;
+
+// i = 0
+@i
+M = 0;
+
+(LOOP)
+// if i >= multiplier, jump to end of loop
+@R1
+D = M
+
+@i
+D = M - D
+
+@END_LOOP
+D; JGE
+
+// result = result + multiplicand
+@R0
+D = M
+
+@R2
+M = M + D
+
+// i++
+@i
+M = M + 1
+
+// Keep looping
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
